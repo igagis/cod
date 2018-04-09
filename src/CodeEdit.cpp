d90e892f4527fd8443c464761bb71a6e2563c96b
Ivan Gagis <igagis@gmail.com>
2018-04-09 15:48:05 +0300

    stuff

---
+++ src/CodeEdit.cpp
@@ -0,0 +1,11 @@
+#include "CodeEdit.hpp"
+
+CodeEdit::CodeEdit(const stob::Node* chain) :
+		Widget(chain)
+{
+	
+}
+
+void CodeEdit::onCharacterInput(const std::u32string& unicode, morda::Key_e key) {
+
+}
