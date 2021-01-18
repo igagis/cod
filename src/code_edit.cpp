bcf7ecd61a693e648493c7d6a87fa7070758b759
Ivan Gagis <igagis@gmail.com>
2021-01-18 16:03:02 +0200

    stuff

--- src/code_edit.cpp
+++ src/code_edit.cpp
@@ -1,12 +1,12 @@
 #include "code_edit.hpp"
 
-CodeEdit::CodeEdit(std::shared_ptr<morda::context> c, const puu::forest& desc) :
+code_edit::code_edit(std::shared_ptr<morda::context> c, const puu::forest& desc) :
 		widget(std::move(c), desc),
 		character_input_widget(this->context)
 {
 	
 }
 
-void CodeEdit::on_character_input(const std::u32string& unicode, morda::key key){
+void code_edit::on_character_input(const std::u32string& unicode, morda::key key){
 
 }
--- src/code_edit.hpp
+++ src/code_edit.hpp
@@ -2,12 +2,12 @@
 
 #include <morda/widgets/character_input_widget.hpp>
 
-class CodeEdit : public morda::character_input_widget{
+class code_edit : public morda::character_input_widget{
 public:
-	CodeEdit(std::shared_ptr<morda::context> c, const puu::forest& desc);
+	code_edit(std::shared_ptr<morda::context> c, const puu::forest& desc);
 	
-	CodeEdit(const CodeEdit&) = delete;
-	CodeEdit& operator=(const CodeEdit&) = delete;
+	code_edit(const code_edit&) = delete;
+	code_edit& operator=(const code_edit&) = delete;
 	
 	
 	void on_character_input(const std::u32string& unicode, morda::key key)override;
