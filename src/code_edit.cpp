be402aa29ccd837c8fb72bcda49eddf5dede3978
Ivan Gagis <igagis@gmail.com>
2021-01-19 17:07:46 +0200

    stuff

--- src/code_edit.cpp
+++ src/code_edit.cpp
@@ -11,3 +11,17 @@ code_edit::code_edit(std::shared_ptr<morda::context> c, const puu::forest& desc)
 void code_edit::on_character_input(const std::u32string& unicode, morda::key key){
 
 }
+
+void code_edit::set_text(std::u32string&& text){
+	// TODO:
+}
+
+std::u32string code_edit::get_text()const{
+	std::u32string ret;
+
+	for(auto& l : this->lines){
+		ret.append(l);
+	}
+
+	return ret;
+}
--- src/code_edit.hpp
+++ src/code_edit.hpp
@@ -1,11 +1,15 @@
 #pragma once
 
+#include <vector>
+
 #include <morda/widgets/character_input_widget.hpp>
 #include <morda/widgets/base/text_widget.hpp>
 class code_edit :
 		public morda::character_input_widget,
 		public morda::text_widget
 {
+	std::vector<std::u32string> lines;
+
 public:
 	code_edit(std::shared_ptr<morda::context> c, const puu::forest& desc);
 	
@@ -13,6 +17,11 @@ public:
 	code_edit& operator=(const code_edit&) = delete;
 	
 	void on_character_input(const std::u32string& unicode, morda::key key)override;
+
+	using morda::text_widget::set_text;
+
+	void set_text(std::u32string&& text)override;
+	std::u32string get_text()const override;
 private:
 
 };
