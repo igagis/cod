85ad84af85814174b98233fd806f0361a12f6571
Ivan Gagis <igagis@gmail.com>
2021-02-28 21:59:23 +0200

    stuff

---
+++ src/application.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <mordavokne/application.hpp>
+
+#include "code_edit.hpp"
+#include "file_tree.hpp"
+
+struct command_line_arguments{
+	std::string base_dir;
+};
+
+class application : public mordavokne::application{
+public:
+	const command_line_arguments cla;
+
+	application(command_line_arguments&& cla);
+};
