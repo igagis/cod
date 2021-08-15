bb1ce5fe524d5b69e8d4ae121326b25171c27787
Ivan Gagis <igagis@gmail.com>
2021-08-15 23:48:21 +0300

    stuff

--- src/cod/syntax_highlight/regex_syntax_highlighter.cpp
+++ src/cod/syntax_highlight/regex_syntax_highlighter.cpp
@@ -19,5 +19,20 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 /* ================ LICENSE END ================ */
 
-#include "regexp_syntax_highlighter.hpp"
+#include "regex_syntax_highlighter.hpp"
 
+using namespace cod;
+
+regex_syntax_highlighter::regex_syntax_highlighter(const treeml::forest& spec){
+
+}
+
+void regex_syntax_highlighter::reset(){
+
+}
+
+std::vector<line_span> regex_syntax_highlighter::highlight(std::u32string_view str){
+    std::vector<line_span> ret;
+
+    return ret;
+}
--- tests/unit/regex_syntax_highlighter.cpp
+++ tests/unit/regex_syntax_highlighter.cpp
@@ -1,8 +1,17 @@
 #include <tst/set.hpp>
 #include <tst/check.hpp>
 
+#include <papki/fs_file.hpp>
+
+#include <cod/syntax_highlight/regex_syntax_highlighter.hpp>
+
 namespace{
 tst::set set("regex_syntax_highlighter", [](tst::suite& suite){
-    
+    suite.add(
+        "parse",
+        [](){
+            cod::regex_syntax_highlighter sh(treeml::read(papki::fs_file("../../highlight/xml.tml")));
+        }
+    );
 });
 }
