881f3bf313d0f493b67c3e43fd0ef32023acced4
Ivan Gagis <igagis@gmail.com>
2021-08-31 22:10:16 +0300

    stuff

--- src/cod/src/tiling_area.hpp
+++ src/cod/src/tiling_area.hpp
@@ -1,10 +1,15 @@
 #pragma once
 
-
+#include <morda/widgets/group/pile.hpp>
+#include <morda/widgets/group/linear_container.hpp>
 
 namespace cod{
 
-class tiling_area{
+class tiling_area :
+        virtual public morda::widget,
+        private morda::pile
+{
+public:
     
 };
 
