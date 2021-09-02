a207195b2cfb6134463a5d0b9b8ebdb97f4d5ceb
Ivan Gagis <igagis@gmail.com>
2021-09-03 00:21:39 +0300

    stuff

--- src/cod/src/tiling_area.cpp
+++ src/cod/src/tiling_area.cpp
@@ -5,4 +5,11 @@ using namespace cod;
 tiling_area::tiling_area(std::shared_ptr<morda::context> c, const treeml::forest& desc) :
         morda::widget(std::move(c), desc),
         morda::container(this->context, treeml::forest())
-{}
+{
+    this->content = std::make_shared<morda::linear_container>(this->context, treeml::forest(), false);
+    this->push_back(this->content);
+}
+
+void tiling_area::push_back(std::shared_ptr<widget> w){
+    this->content->push_back(std::move(w));
+}
--- src/cod/src/tiling_area.hpp
+++ src/cod/src/tiling_area.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <morda/widgets/container.hpp>
+#include <morda/widgets/group/linear_container.hpp>
 
 namespace cod{
 
@@ -8,8 +9,13 @@ class tiling_area :
         virtual public morda::widget,
         private morda::container
 {
+    std::shared_ptr<morda::linear_container> content;
 public:
     tiling_area(std::shared_ptr<morda::context> c, const treeml::forest& desc);
+
+    void push_back(std::shared_ptr<widget> w);
+
+    // TODO: override layout and measure
 };
 
 }
