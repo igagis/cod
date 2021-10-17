84057c7573f8a16dbeb839b15cc601c0648af107
Ivan Gagis <igagis@gmail.com>
2021-10-18 01:40:46 +0300

    stuff

--- src/plugins/text_editor/src/plugin.cpp
+++ src/plugins/text_editor/src/plugin.cpp
@@ -1,1 +1,7 @@
 #include "plugin.hpp"
+
+using namespace cod;
+
+std::shared_ptr<morda::page> text_editor_plugin::open_file(std::string_view file_name){
+    return nullptr;
+}
--- src/plugins/text_editor/src/plugin.hpp
+++ src/plugins/text_editor/src/plugin.hpp
@@ -5,7 +5,8 @@
 namespace cod{
 
 class text_editor_plugin : public cod::plugin{
-
+public:
+    std::shared_ptr<morda::page> open_file(std::string_view file_name)override;
 };
 
 }
