f709e5a00ef20e773a99a84dc23347d28de3b887
Ivan Gagis <igagis@gmail.com>
2021-11-18 17:04:33 +0200

    stuff

--- src/libcod/src/cod/shortcut_resolver.cpp
+++ src/libcod/src/cod/shortcut_resolver.cpp
@@ -1,3 +1,11 @@
 #include "shortcut_resolver.hpp"
 
 using namespace cod;
+
+void shortcut_resolver::load(const papki::file& f){
+	// TODO:
+}
+
+const shortcut_resolver::shortcut& shortcut_resolver::get(const std::string& name)const{
+	return this->shortcuts[name];
+}
--- src/libcod/src/cod/shortcut_resolver.hpp
+++ src/libcod/src/cod/shortcut_resolver.hpp
@@ -1,9 +1,29 @@
 #pragma once
 
+#include <unordered_map>
+
+#include <utki/flags.hpp>
+#include <papki/file.hpp>
+
+#include <morda/util/key.hpp>
+
 namespace cod{
 
 class shortcut_resolver{
 
+public:
+	struct shortcut{
+		utki::flags<morda::key_modifier> modifiers;
+		morda::key key = morda::key::unknown;
+	};
+
+private:
+	mutable std::unordered_map<std::string, shortcut> shortcuts;
+
+public:
+	void load(const papki::file& f);
+
+	const shortcut& get(const std::string& name)const;
 };
 
 }
