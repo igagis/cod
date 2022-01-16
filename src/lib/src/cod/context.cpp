cae50a650f0cce865e1fd0776a60637069e9f3af
Ivan Gagis <igagis@gmail.com>
2022-01-16 13:27:00 +0200

    stuff

--- src/lib/src/cod/context.cpp
+++ src/lib/src/cod/context.cpp
@@ -6,8 +6,6 @@ context::context(command_line_args&& cla, mordavokne::application& app) :
         base_dir(std::move(cla.base_dir)),
         res_file(app.get_res_file()),
         gui(app),
-		shortcuts(*app.get_res_file("res/shortcuts.tml")),
+		shortcuts(*app.get_res_file("res/shortcuts.tml")), // TODO: how to inject shortcuts filename?
         plugins(cla.plugins)
-{
-
-}
+{}
